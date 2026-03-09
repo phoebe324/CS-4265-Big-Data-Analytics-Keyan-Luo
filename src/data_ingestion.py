ab933b4f2542395247d691e78009613ab134525e
phoebe324 <161937427+phoebe324@users.noreply.github.com>
2026-03-09 07:12:26 -0400

    Delete src/data_ingestion.py

--- src/data_ingestion.py
+++
@@ -1,9 +0,0 @@
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
-
-# Load the latest version of the dataset
-df = kagglehub.load_dataset(
-  KaggleDatasetAdapter.PANDAS,
-  "vishardmehta/faang-stock-market-data-with-technical-indicators",
-  ""
-)
